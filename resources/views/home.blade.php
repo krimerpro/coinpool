dfd96c2907526035368d28348b7eca9157b4e52d
varanustoken <varanustoken@gmail.com>
2023-04-05 21:25:48 +0700

    a

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -26,8 +26,8 @@
                     <h1>Trading platform of the <span>future!</span></h1>
                     <p class="lead">Advanced protocol based on smart chain ecosystem</p>
                     <div class="hero-btns">
-                        <a href="#" class="btn">JOIN US</a>
-                        <a href="#" class="btn btn3">POOCOIN CHART</a>
+                        <a href="https://t.me/CoinpoolProtocol" class="btn">JOIN US</a>
+                        <a href="https://poocoin.app/tokens/0x46aa42492cd27e4210bf6b5ed5ddfce44a705c0b" class="btn btn3">POOCOIN CHART</a>
                     </div>
                 </div>
             </div>
